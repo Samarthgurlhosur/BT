537bd0d51b01b3a5676e61d5cf0fd6f90d39fd02
Samarthgurlhosur <149358346+Samarthgurlhosur@users.noreply.github.com>
2026-04-16 00:40:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -793,4 +793,4 @@ def seed_data():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socket_io.run(app, debug=True, host='0.0.0.0', port=5000)
+    socket_io.run(app, debug=False, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
